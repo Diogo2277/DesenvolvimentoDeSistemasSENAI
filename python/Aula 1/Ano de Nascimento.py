c1d61319ebcca0e9fcf532c7fe4d649b69e586d3
Diogo2277 <diogo.s.oliveira@aluno.senai.br>
2024-11-14 08:27:28 -0300

    Atividade 1
    Ano de Nascimento1!

---
+++ python/Aula 1/Ano de Nascimento.py
@@ -0,0 +1,5 @@
+ano_atual = int(input('Qual é o ano atual?'))
+ano_nascimento = int(input('Qual é o ano em que você nasceu?'))
+idade = ano_atual - ano_nascimento
+
+print ('Sua idade é:', idade )
---
+++ python/Aula 1/Nome, email e senha.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome: ')
+email = input('Digite seu email: ')
+senha = input('Digite sua senha: ')
+
+print(f' Nome do usuario {nome} ') 
+print(f' Email do usuario {email} ')
+print(f' Senha do usario "{senha}" ')
