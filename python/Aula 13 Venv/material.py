0fa674a75bfd85b9d4750d87d2640873e4683fce
Diogo2277 <diogo.s.oliveira@aluno.senai.br>
2024-12-02 11:16:39 -0300

    Aula venv

---
+++ python/Aula 13 Venv/material.py
@@ -0,0 +1,30 @@
+'''
+CODIGO PARA PUXAR A VENV
+python -m venv venv
+
+ATIVAR VENV .\ endereço do activate
+
+DECOBRIR QUAL O PYTHON ESTOU UTILIZANDO
+gcm python -Syntax
+
+LIMPAR O TERMINAL 
+cls
+
+INSTALAR PIP
+pip install
+
+INSTALAR PYMYSQL
+pip install pymysql
+
+DESISNTALAR PYMYSQL
+pip uninstall pymysql
+
+CRIAR REQUIREMENTS 
+pip freeze > requirements.txt
+
+INSTALAR REQUIRIMENTS
+pip install -r requirements.txt
+
+DESATIVAR 
+deactivate
+'''
